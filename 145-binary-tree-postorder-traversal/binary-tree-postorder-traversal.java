bacb929734930454d69f05794110cf94f49b6089
Dipankar Dutta <deepankar.dutta1@gmail.com>
2024-06-18 07:40:15 +0530

    Time: 0 ms (100.00%) | Memory: 41.6 MB (32.51%) - LeetSync

--- 145-binary-tree-postorder-traversal/binary-tree-postorder-traversal.java
+++ 145-binary-tree-postorder-traversal/binary-tree-postorder-traversal.java
@@ -15,24 +15,56 @@
  */
 class Solution {
    
+
+   // using recursion 
         
-      public List<Integer> postorderTraversal(TreeNode root) {
-        if(root==null){
-            return new  ArrayList<Integer>();
-        }
+    //   public List<Integer> postorderTraversal(TreeNode root) {
+    //     if(root==null){
+    //         return new  ArrayList<Integer>();
+    //     }
+    //     List<Integer> ansList=new ArrayList<>();
+    //     postOrderTraverse(root,ansList);
+    //     return ansList;
+
+    // }
+    // public void postOrderTraverse(TreeNode root,List<Integer> ansList){
+    //      if(root==null){
+    //         return ;
+    //     }
+    //     postOrderTraverse(root.left,ansList);
+    //     postOrderTraverse(root.right,ansList);
+    //     ansList.add(root.val);
+
+
+    // }
+
+
+    //using 1 stack 
+    public List<Integer> postorderTraversal(TreeNode root) {
+        Stack<TreeNode> st=new Stack<>();
         List<Integer> ansList=new ArrayList<>();
-        postOrderTraverse(root,ansList);
+        st.push(root);
+        postOrderTraverse(ansList,st);
+        // answer will come in reverse order if used two stack then it would come in correct order
+        Collections.reverse(ansList); 
         return ansList;
 
     }
-    public void postOrderTraverse(TreeNode root,List<Integer> ansList){
-         if(root==null){
-            return ;
-        }
-        postOrderTraverse(root.left,ansList);
-        postOrderTraverse(root.right,ansList);
-        ansList.add(root.val);
+     public void postOrderTraverse(List<Integer> ansList,Stack<TreeNode> st){
 
+            if(st.isEmpty()){
+                return;
+            }
 
-    }
+            TreeNode temp=st.pop();
+            //whenever pops add to ansList
+            if(temp!=null)
+            ansList.add(temp.val);
+            if(temp!=null && temp.left!=null)
+            st.push(temp.left);
+            if(temp!=null && temp.right!=null)
+            st.push(temp.right);
+            postOrderTraverse(ansList,st);
+
+     }
 }
