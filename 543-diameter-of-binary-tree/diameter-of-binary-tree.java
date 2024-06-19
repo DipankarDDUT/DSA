de6997117d650ac0d277f99ed3e11daa4d4bab64
Dipankar Dutta <deepankar.dutta1@gmail.com>
2024-06-19 09:17:36 +0530

    Time: 0 ms (100.00%) | Memory: 44.2 MB (96.29%) - LeetSync

--- 543-diameter-of-binary-tree/diameter-of-binary-tree.java
+++ 543-diameter-of-binary-tree/diameter-of-binary-tree.java
@@ -13,44 +13,74 @@
  *     }
  * }
  */
+
+ /**
+ Problem with this brute force is first each node looping over entire tree
+ so 0(n2)
+  */
+// class Solution {
+//     public int maxHeight=0;
+//     public int diameterOfBinaryTree(TreeNode root) {
+//         max(root);
+//         return maxHeight;
+//     }
+
+//     public int max(TreeNode root){
+//         if(root==null){
+//             return maxHeight;
+//         }
+//         int leftH=findHeight(root.left);
+//         int rightH=findHeight(root.right);
+//              if(maxHeight<leftH+rightH){
+//             maxHeight=leftH+rightH;
+//         }
+//         // System.out.println(root.val);
+//         System.out.println(leftH+rightH);
+//         // System.out.println(rightH);
+//         maxHeight=max(root.left);
+//         maxHeight=max(root.right);
+     
+//         return maxHeight;
+
+//     }
+
+//     public int findHeight(TreeNode node){
+//         int tempL=0;
+//         int tempR=0;
+//         if(node==null){
+//             return 0;
+//         }
+//          tempL=1+ findHeight(node.left);
+//          tempR=1+findHeight(node.right);
+
+//          return tempL>tempR?tempL:tempR;
+//     }
+
+   
+
+// }
+//Optimal aprroach 
 class Solution {
     public int maxHeight=0;
     public int diameterOfBinaryTree(TreeNode root) {
-        max(root);
+        findMax(root);
         return maxHeight;
     }
 
-    public int max(TreeNode root){
-        if(root==null){
-            return maxHeight;
-        }
-        int leftH=findHeight(root.left);
-        int rightH=findHeight(root.right);
-             if(maxHeight<leftH+rightH){
-            maxHeight=leftH+rightH;
-        }
-        // System.out.println(root.val);
-        System.out.println(leftH+rightH);
-        // System.out.println(rightH);
-        maxHeight=max(root.left);
-        maxHeight=max(root.right);
-     
-        return maxHeight;
-
-    }
-
-    public int findHeight(TreeNode node){
-        int tempL=0;
-        int tempR=0;
+    public int findMax(TreeNode node){
         if(node==null){
             return 0;
         }
-         tempL=1+ findHeight(node.left);
-         tempR=1+findHeight(node.right);
+        int lh=findMax(node.left);
+        int rh=findMax(node.right);
 
-         return tempL>tempR?tempL:tempR;
+        if(maxHeight<lh+rh){
+            maxHeight=lh+rh;
+        }
+        if(lh>rh){
+            return 1 + lh;
+        }
+        return 1 + rh;
     }
-
-   
 
 }
