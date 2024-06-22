0c3ba48b329ff053dcae000b49a06b48fc3d123c
Dipankar Dutta <deepankar.dutta1@gmail.com>
2024-06-22 12:38:26 +0530

    Time: 0 ms (100.00%) | Memory: 41.3 MB (39.27%) - LeetSync

--- 232-implement-queue-using-stacks/implement-queue-using-stacks.java
+++ 232-implement-queue-using-stacks/implement-queue-using-stacks.java
@@ -1,37 +1,85 @@
 import java.util.*;
+// class MyQueue {
+
+//     int cur;
+//     Stack<Integer> st=new Stack<>(); 
+//     Stack<Integer> stBackUp=new Stack();
+//     public MyQueue() {
+//         cur=0;
+//     }
+    
+//     // here push operation is O(n)  we can do better can make it O(1) like pop operation and peek operation 
+//     public void push(int x) {
+//         while(!st.isEmpty()){
+//             stBackUp.push(st.pop());
+//         }
+//         st.push(x);
+//          while(!stBackUp.isEmpty()){
+//             st.push(stBackUp.pop());
+//         }
+//         cur++;
+//     }
+    
+//     public int pop() {
+//         if(st.isEmpty()){
+//             return -1;
+//         }
+//         return st.pop();
+//     }
+    
+//     public int peek() {
+//         return st.peek();
+//     }
+    
+//     public boolean empty() {
+//         return st.isEmpty();
+//     }
+
+// }
+
+// here pop may be O(n) or 1 if output empty than O(n) else 1 same for peek
 class MyQueue {
 
     int cur;
-    Stack<Integer> st=new Stack<>(); 
-    Stack<Integer> stBackUp=new Stack();
+    Stack<Integer> input=new Stack<>(); 
+    Stack<Integer> output=new Stack();
     public MyQueue() {
         cur=0;
     }
     
     public void push(int x) {
-        while(!st.isEmpty()){
-            stBackUp.push(st.pop());
-        }
-        st.push(x);
-         while(!stBackUp.isEmpty()){
-            st.push(stBackUp.pop());
-        }
+
+        input.push(x);
+        cur++;
     }
     
     public int pop() {
-        if(st.isEmpty()){
+        if(input.isEmpty() && output.isEmpty()){
             return -1;
         }
-        return st.pop();
+        if(output.isEmpty()){
+            while(!input.isEmpty()){
+                output.push(input.pop());
+            }
+        }
+        return output.pop();
     }
     
     public int peek() {
-        return st.peek();
+        if(!output.isEmpty()){
+            return output.peek();
+        }else{
+            while(!input.isEmpty()){
+                output.push(input.pop());
+            }
+              return output.peek();
+        }
     }
     
     public boolean empty() {
-        return st.isEmpty();
+        return input.isEmpty() && output.isEmpty();
     }
+
 }
 
 /**
