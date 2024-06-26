03dcd329e773a5e1f738a04c9f51dbe413847494
Dipankar Dutta <deepankar.dutta1@gmail.com>
2024-06-26 08:16:45 +0530

    Time: 17 ms (45.82%) | Memory: 46.3 MB (29.59%) - LeetSync

---
+++ 503-next-greater-element-ii/next-greater-element-ii.java
@@ -0,0 +1,35 @@
+class Solution {
+    public int[] nextGreaterElements(int[] nums) {
+        
+        Stack<Integer> st=new Stack<>();
+        int result[]=new int[nums.length];
+        for(int i=(2 * nums.length)-1;i>=0;i--){
+            // as usual start from last  1 2 1 1 2 1 last 3 are imaginary
+            int index=0;
+            if(i>=nums.length){
+                index=i-nums.length;
+            }else{
+                index=i;
+            }
+            while(!st.isEmpty() && st.peek()<=nums[index]){
+                st.pop();
+            }
+            // add to result only if nums.length
+            if(!st.isEmpty() && i<nums.length ){
+                    result[index]=st.peek();
+            }else if (st.isEmpty() && i<nums.length ){
+                result[index]=-1;
+            }
+            // if(!st.isEmpty()){
+            // if(st.peek()<nums[index]){
+            st.push(nums[index]);
+            // }
+            // }else{
+            //     st.push(nums[index]);
+            // }
+
+        }
+
+        return result;
+    }
+}
