7fe01c2f241941644af11f83f014cc0cd243db9a
Dipankar Dutta <deepankar.dutta1@gmail.com>
2025-04-01 00:29:03 +0530

    Time: 0 ms (100.00%) | Memory: 41.6 MB (96.48%) - LeetSync

--- 75-sort-colors/sort-colors.java
+++ 75-sort-colors/sort-colors.java
@@ -1,36 +1,41 @@
 class Solution {
     public void sortColors(int[] nums) {
-        
 
-            int red=0;
-            int white=0;
-            int size=nums.length-1;
-            int blue=size;
-
-            while(white<=blue){
-                if(nums[white]==0){
-                    //next will be always be pointing to 1 other than when in index as 0 
-                    // as ot will be on next pointer 
-                  int temp=nums[white];
-                    nums[white]=nums[red];
-                    nums[red]=temp;
-                    white++;
-                    red++;
-
-                }else if(nums[white]==1){
-                    white++;
-                }else{
-                    //its 2
-                        // here white ++ or -- cannot as not sure what just swap can be 0 
-                        // but for when 0 then ofcourse previous is 1 or 0
-                   int  temp=nums[white];
-                    nums[white]=nums[blue];
-                    nums[blue]=temp;
-                    blue--;
-                }
+        //Brute
+        int i=0,j=0,k=0; 
+        for(int l=0;l<nums.length;l++){
+            if(nums[l]==0){
+                i++;
+            }else if(nums[l]==1){
+                j++;
+            }else{
+                k++;
             }
+        }
+        int index=0;
+        int l=0;
+        while(l<i){
+            nums[index]=0;
+            l++;
+            index++;
+        }
+        l=0;
+           while(l<j){
+            nums[index]=1;
+            l++;
+            index++;
+        }
+         l=0;
+           while(l<k){
+            nums[index]=2;
+            l++;
+            index++;
+        }
 
 
-
+        //Optimal
+        // int zero=0;
+        // int one=0;
+        // int two=nums.length-1;
     }
 }
