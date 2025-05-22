e4b10e01a47b7b8c7aaaf710e33fc1d162d29337
Dipankar Dutta <deepankar.dutta1@gmail.com>
2025-05-23 00:08:43 +0530

    Time: 1 ms (92.73%) | Memory: 45.7 MB (35.13%) - LeetSync

--- 724-find-pivot-index/find-pivot-index.java
+++ 724-find-pivot-index/find-pivot-index.java
@@ -1,26 +1,59 @@
 class Solution {
     public int pivotIndex(int[] nums) {
-        int pivot =0;
 
+        // Brute force 
+        // int pivot =0;
+
+        // while(pivot<nums.length){
+        //     // till second last
+        //     int sumLeft=0;
+        //     int sumRight=0;
+        //     for(int i=0;i<pivot;i++){
+        //         sumLeft+=nums[i];
+        //     }
+        //      for(int j=pivot+1;j<nums.length;j++){
+        //         sumRight+=nums[j];
+        //     }
+        //     // if(sumLeft==0 && sumRight==0){
+        //     //     return 0;
+        //     // }
+        //     if(sumLeft==sumRight){
+        //         return pivot;
+        //     }
+        //     pivot++;
+        // }
+
+        // return -1;
+
+           int pivot =1;
+           int sumLeft=0;
+           int sumRight=0;
+           for(int i=1;i<nums.length;i++){
+            // taking 1st index as pivot
+                sumRight+=nums[i];
+           }
+        if(sumLeft==sumRight){
+                return 0;
+            }
         while(pivot<nums.length){
-            // till second last
-            int sumLeft=0;
-            int sumRight=0;
-            for(int i=0;i<pivot;i++){
-                sumLeft+=nums[i];
-            }
-             for(int j=pivot+1;j<nums.length;j++){
-                sumRight+=nums[j];
-            }
-            // if(sumLeft==0 && sumRight==0){
-            //     return 0;
-            // }
-            if(sumLeft==sumRight){
+    
+            // add prev pivot as pivot not yet updated
+            sumLeft+=nums[pivot-1];
+            sumRight-=nums[pivot];
+                    if(sumLeft==sumRight){
                 return pivot;
             }
             pivot++;
+
+ 
+
         }
 
         return -1;
+
+
+        
+        
+
     }
 }
