07b98a39460de54d811b2c37af565be64b70c933
Dipankar Dutta <deepankar.dutta1@gmail.com>
2025-06-08 14:11:11 +0530

    Time: 0 ms (100.00%) | Memory: 45 MB (60.52%) - LeetSync

--- 941-sort-array-by-parity/sort-array-by-parity.java
+++ 941-sort-array-by-parity/sort-array-by-parity.java
@@ -1,31 +1,20 @@
 class Solution {
-    public void swap(int[] nums, int m , int n){
-        if(nums[m]==nums[n]||m==n)return;
-        int temp=nums[m];
-        nums[m]=nums[n];
-        nums[n]=temp;
-    }
     public int[] sortArrayByParity(int[] nums) {
-        // Brtue will extra same array 
-        // loop through the array and add the evn number only and keep the count
-        // then cnt + 1 from index  start adding loop again to add odd 
-        // then copy the whole array back 
-        // time O(3n) space O(n)
-
-        if(nums.length==0||nums.length==1){
+        
+        if(nums.length<2){
             return nums;
         }
         int even=0;
         int odd=nums.length-1;
         while(even<odd){
-            if(nums[even]%2==0){
-                even++;
-            }
-            else if(nums[odd]%2!=0){
+            if(nums[even]%2!=0){
+                int temp=nums[odd];
+                nums[odd]=nums[even];
+                nums[even]=temp;
                 odd--;
             }else{
-            swap(nums,even,odd);
-
+       
+                even++;
             }
         }
 
