f719af74200e5af50fca5cdaf5afefddd6697dc4
Dipankar Dutta <deepankar.dutta1@gmail.com>
2025-06-08 18:22:11 +0530

    Time: 0 ms (100.00%) | Memory: 41.1 MB (95.97%) - LeetSync

--- 1137-height-checker/height-checker.java
+++ 1137-height-checker/height-checker.java
@@ -1,37 +1,65 @@
 class Solution {
     public int heightChecker(int[] heights) {
-        
-        // it sees number of swaps + 1 is the answer
-        int left=0;
-        int right=heights.length-1;
-        int[] expected=new int[heights.length];
-     for(int i=0;i<heights.length;i++){
-            expected[i]=heights[i];
-            // due to already writte here input heights should nto be changed 
-            // expected should change anyways 
-        }
-        // sorting in n
-        // while(left<right){
-        //     if(heights[left]>=heights[right]){
-        //         int temp=heights[left];
-        //         heights[left]=heights[right];
-        //         heights[right]=temp;
-        //         right--;
-        //     }else{
-        //         left++;
-        //     }
-        // }
-        Arrays.sort(heights);
-
-
-        int no_of_misPlace=0;
+     
+        int largest=Integer.MIN_VALUE;
         for(int i=0;i<heights.length;i++){
-            if(heights[i]!=expected[i]){
-                no_of_misPlace++;
+            if(heights[i]>largest){
+                largest=heights[i];
             }
         }
+        int[] freqArr=new int[largest+1];
+        int mismatch=0;
 
-        return no_of_misPlace;
+        // lets create the freqArray
+        for(int i=0;i<heights.length;i++){
+            freqArr[heights[i]]+=1;
+        }
+        int index=0;
+        for(int i=1;i<freqArr.length;i++){
+            while(freqArr[i]!=0){
+                if(heights[index]!=i){
+                    mismatch++;
+                }
+                freqArr[i]--;
+                index++;
+            }
+
+        }
+
+        return mismatch;
+
+
+
+
+
+
+    //  for(int i=0;i<heights.length;i++){
+    //         expected[i]=heights[i];
+    //         // due to already writte here input heights should nto be changed 
+    //         // expected should change anyways 
+    //     }
+    //     // sorting in n
+    //     // while(left<right){
+    //     //     if(heights[left]>=heights[right]){
+    //     //         int temp=heights[left];
+    //     //         heights[left]=heights[right];
+    //     //         heights[right]=temp;
+    //     //         right--;
+    //     //     }else{
+    //     //         left++;
+    //     //     }
+    //     // }
+    //     Arrays.sort(heights);
+
+
+    //     int no_of_misPlace=0;
+    //     for(int i=0;i<heights.length;i++){
+    //         if(heights[i]!=expected[i]){
+    //             no_of_misPlace++;
+    //         }
+    //     }
+
+    //     return no_of_misPlace;
 
    
 
