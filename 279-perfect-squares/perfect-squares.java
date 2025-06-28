f8105b908bea1c40e22a37854ed6429e1127086d
Dipankar Dutta <deepankar.dutta1@gmail.com>
2025-06-28 09:19:47 +0530

    Time: 21 ms (92.65%) | Memory: 45.1 MB (17.98%) - LeetSync

--- 279-perfect-squares/perfect-squares.java
+++ 279-perfect-squares/perfect-squares.java
@@ -9,27 +9,22 @@ class Solution {
         boolean[] visited=new boolean[n+1];
         int steps=0;
         qu.offer(0);
+        visited[0]=true;
 
         while(!qu.isEmpty()){
             int size=qu.size();
             for(int j=0;j<size;j++){
-                int cur=qu.peek();
+                int cur=qu.poll();
                 if(cur==n){
                     return steps;
                 }
                 for(int i=1;i*i+cur<=n;i++){
                     int curN=(i*i)+cur;
-                    if(curN<=n){
                         if(!visited[curN]){
                         qu.offer(curN);
                         visited[curN]=true;
                         }
-                        
-                    }else{
-                        break;
-                    }
                 }
-                qu.poll();
             }
                 steps++;
 
