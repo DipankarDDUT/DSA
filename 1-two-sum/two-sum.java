851f662bd08f5d6018fd83304bac8fbf99bf1635
Dipankar Dutta <deepankar.dutta1@gmail.com>
2025-08-05 06:18:41 +0530

    Time: 2 ms (98.97%) | Memory: 45.2 MB (18.38%) - LeetSync

--- 1-two-sum/two-sum.java
+++ 1-two-sum/two-sum.java
@@ -1,47 +1,23 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
+        int[] result=new int[2];
 
-        // // space n and time nlogn
-        // int[] temp=Arrays.copyOf(nums,nums.length);
-        // Arrays.sort(nums);
-        // int i=0;
-        // int j=nums.length-1;
-        // int num1=-1;
-        // int num2=-1;
-        // while(i<j){
-        //     if(nums[i]+nums[j]==target){
-        //         System.out.println(nums[i]+"\t"+nums[j]);
-        //         break;
-        //     }else if(nums[i]+nums[j]>target){
-        //         j--;
-        //     }else{
-        //         i++;
-        //     }
-        // }
-        // // i and j still holds the target sum of sorted array
-        // for(int k=0;k<temp.length;k++){
-        //     if(temp[k]==nums[i] && num1==-1){
-        //         num1=k;
-        //     }else if(temp[k]==nums[j] && num2==-1){
-        //         num2=k;
-        //     }
-        // }
-
-
-        // return new int[]{num1,num2};
-
-        // number , index
         HashMap<Integer,Integer> preMap=new HashMap<>();
+        // value index
 
         for(int i=0;i<nums.length;i++){
-            preMap.put(nums[i],i);
-        } 
-
-        for(int i=0;i<nums.length;i++){
-            if(preMap.containsKey(target-nums[i]) && preMap.get(target-nums[i])!=i){
-                return new int[]{i,preMap.get(target-nums[i])};
+            int remain=target-nums[i];
+            if(preMap.containsKey(remain)){
+                result[0]=i;
+                result[1]=preMap.get(remain);
+                return result;
+            }else{
+                preMap.put(nums[i],i);
             }
         }
-        return new int[]{};
+
+        return result;
+
+
     }
 }
