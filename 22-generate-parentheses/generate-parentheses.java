427f07273c83bd4a2c38e159e5d97b2c25168252
Dipankar Dutta <deepankar.dutta1@gmail.com>
2025-08-13 17:19:47 +0530

    Time: 2 ms (47.83%) | Memory: 43.5 MB (35.48%) - LeetSync

---
+++ 22-generate-parentheses/generate-parentheses.java
@@ -0,0 +1,29 @@
+class Solution {
+    public List<String> generateParenthesis(int n) {
+        List<String> result=new ArrayList<>();
+        String s="(";
+        generate(n,1,s,1,0,result);
+        return result;
+    }
+
+    public void generate(int n,int index,String s,int open, int close, List<String> result){
+
+        if(open==close && index==2 * n){
+            result.add(s);
+            return;
+        }
+
+        if(open>n || close>n){
+            // as parenthesis need to be balanced
+            // as open crossed the midway
+            return ;
+        }
+
+        if(open<n){
+            generate(n,index+1,s+"(",open+1,close,result);
+        }
+        if(open>close){
+            generate(n,index+1,s+")",open,close+1,result);
+        }
+    }
+}
