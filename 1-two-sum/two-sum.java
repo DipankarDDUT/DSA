dd287d1d5d0d2123a3feb16217832eb6993833a0
Dipankar Dutta <deepankar.dutta1@gmail.com>
2025-08-28 08:43:09 +0530

    Time: 2 ms (98.89%) | Memory: 45.4 MB (10.15%) - LeetSync

--- 1-two-sum/two-sum.java
+++ 1-two-sum/two-sum.java
@@ -1,23 +1,34 @@
 class Solution {
-    public int[] twoSum(int[] nums, int target) {
-        int[] result=new int[2];
+  public int[] twoSum(int[] nums, int target) {
 
-        HashMap<Integer,Integer> preMap=new HashMap<>();
-        // value index
+    // brute
+    // will O(n2)
+    // check for one element each elements in the array two loops 
 
-        for(int i=0;i<nums.length;i++){
-            int remain=target-nums[i];
-            if(preMap.containsKey(remain)){
-                result[0]=i;
-                result[1]=preMap.get(remain);
-                return result;
-            }else{
-                preMap.put(nums[i],i);
-            }
+    // Space O(n)
+    // TIme O(n) + O(n)
+    // Value , index
+    HashMap<Integer, Integer> preMap = new HashMap<>();
+    int[] result = new int[2];
+    for (int i = 0; i < nums.length; i++) {
+      int num =nums[i];
+      int moreNeeded=target-num;
+      // no need to check for same index same iteattion it will check all 
+      // previous entry
+      if (preMap.containsKey(moreNeeded)) {
+        if (nums[i] < moreNeeded) {
+          result[0] = i;
+          result[1] = preMap.get(moreNeeded);
+        } else {
+          result[1] = i;
+          result[0] = preMap.get(moreNeeded);
         }
-
         return result;
-
-
+      }else{
+         preMap.put(nums[i], i);
+      }
     }
+
+    return new int[] {-1, -1};
+  }
 }
