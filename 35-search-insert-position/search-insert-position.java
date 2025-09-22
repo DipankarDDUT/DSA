479de8733e10e947d372b4f4f584502843e0ede0
Dipankar Dutta <deepankar.dutta1@gmail.com>
2025-09-23 00:12:36 +0530

    Time: 0 ms (100.00%) | Memory: 43.3 MB (28.31%) - LeetSync

--- 35-search-insert-position/search-insert-position.java
+++ 35-search-insert-position/search-insert-position.java
@@ -1,25 +1,28 @@
 class Solution {
     public int searchInsert(int[] nums, int target) {
-        
-        // Brute force
-        // as array is already sorted
-        for(int i=0;i<nums.length;i++){
-            if(nums[i]==target){
-                return i;
-            }else{
-                if(nums[i]>target){
-                    return i;
+        return getInputIndex(nums,0,nums.length-1,target,nums.length);
+    }
+
+    public int getInputIndex(int[] nums, int low,int high, int target,int ans){
+        // ans will change if bigger than ans and with the smallest index 
+        // upper bound
+        if(high>=low){
+            int mid=low+((high-low)/2);
+            if(target==nums[mid]){
+                ans=mid;
+                return mid;
+            }
+            else if(nums[mid]>target){
+                if(ans>mid){
+                    ans=mid;
                 }
+                return getInputIndex(nums,low,mid-1,target,ans);
+            }else{
+                return getInputIndex(nums,mid+1,high,target,ans);
+
             }
         }
-        
 
-        // target should be +1 index of the array 
-        return nums.length;
-
-        
-        // if not found either will be on the either of the end 
-
-
+        return ans;
     }
 }
