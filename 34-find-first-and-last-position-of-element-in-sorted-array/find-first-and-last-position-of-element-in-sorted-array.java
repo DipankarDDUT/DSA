51adcc43e8a2f9ff6f48b18adb5fe2fa4a3056c1
Dipankar Dutta <deepankar.dutta1@gmail.com>
2025-09-23 08:05:04 +0530

    Time: 0 ms (100.00%) | Memory: 45.8 MB (85.78%) - LeetSync

--- 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.java
+++ 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.java
@@ -2,43 +2,51 @@ class Solution {
   public int[] searchRange(int[] nums, int target) {
 
     // iteration
-    int firstOccurance = getFirstOccurrence(nums,0,nums.length-1,target,-1);
+    int firstOccurrence = getFirstOccurrence(nums, target);
 
+    if (firstOccurrence == -1) {
+      return new int[] {-1, -1};
+    }
+    // if there is no occurrence itself means no number
 
-    int lastOccurrence = getLastOccurrence(nums,0,nums.length-1,target,-1);
+    int lastOccurrence = getLastOccurrence(nums, target);
 
-
-    return new int[] {firstOccurance, lastOccurrence};
+    return new int[] {firstOccurrence, lastOccurrence};
   }
 
-   public int getFirstOccurrence(int[] nums, int low, int high, int target, int ans) {
-    if (high >= low) {
+  public int getFirstOccurrence(int[] nums, int target) {
+    int high = nums.length - 1;
+    int low = 0;
+    int ans = -1;
+    while (high >= low) {
       int mid = low + ((high - low) / 2);
       if (nums[mid] == target) {
         ans = mid;
-        // go to previous element to find more first occurrences
-        return getFirstOccurrence(nums, low, mid - 1, target, ans);
+        high = mid - 1;
       } else if (nums[mid] < target) {
-        return getFirstOccurrence(nums, mid + 1, high, target, ans);
+        low = mid + 1;
       } else {
-        return getFirstOccurrence(nums, low, mid - 1, target, ans);
+        high = mid - 1;
       }
     }
 
     return ans;
   }
 
-     public int getLastOccurrence(int[] nums, int low, int high, int target, int ans) {
-    if (high >= low) {
+  public int getLastOccurrence(int[] nums, int target) {
+    int high = nums.length - 1;
+    int low = 0;
+    int ans = -1;
+    while (high >= low) {
       int mid = low + ((high - low) / 2);
       if (nums[mid] == target) {
         ans = mid;
-        // go to previous element to find more last occurrences
-       return getLastOccurrence(nums, mid + 1, high, target, ans);
+        // go to next element to find more last occurrences
+        low = mid + 1;
       } else if (nums[mid] < target) {
-        return getLastOccurrence(nums, mid + 1, high, target, ans);
+        low = mid + 1;
       } else {
-        return getLastOccurrence(nums, low, mid - 1, target, ans);
+        high = mid - 1;
       }
     }
 
