947e49c4fc87168d7b52265b91ad26a1f2f20a52
Dipankar Dutta <deepankar.dutta1@gmail.com>
2025-09-26 06:50:48 +0530

    Time: 0 ms (100.00%) | Memory: 50.4 MB (42.20%) - LeetSync

--- 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.java
+++ 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.java
@@ -1,19 +1,50 @@
 class Solution {
-    public int singleNonDuplicate(int[] nums) {
+  public int singleNonDuplicate(int[] nums) {
 
-     // lets solve it in O(n)
-     // check each pair 
-     // there may be case 1 left after all the pair 
-    int i=0;
-     while(i<nums.length-1){
-        if(nums[i]!=nums[i+1]){
-            return nums[i];
-        }
-        i=i+2;
-     }
-
-        // as still not found then  last element is ans 
-        return nums[i];
+    if (nums.length == 0) {
+      return -1;
+    }
+    // Edge Cases
+    // single element
+    if (nums.length == 1) {
+      return nums[0];
     }
 
+    // now to reduce the edge cases check in loop again and again
+    // previous elemnt exisit ot not . right side element exist or not
+    // to remove those checks will check the leftmost and rightmost
+    // as edge cases only
+    if (nums[0] != nums[1]) {
+      return nums[0];
+    }
+
+    if (nums[nums.length - 1] != nums[nums.length - 2]) {
+      return nums[nums.length - 1];
+    }
+
+    int low = 1;
+    int high = nums.length - 2;
+
+    while (low <= high) {
+
+      int mid = low+((high - low) / 2);
+      if (nums[mid + 1] != nums[mid] && nums[mid] != nums[mid - 1]) {
+        return nums[mid];
+      }
+      // if element in the odd index and -1 is same element oR
+      // element is in even index and + 1 is the same element
+      // means order not yet distorted , single element is in right side
+      if ((mid % 2 == 1 && nums[mid] == nums[mid - 1]) || (mid%2==0 && nums[mid] == nums[mid + 1])) {
+        low = mid + 1;
+      } else {
+
+        // element is in even index  -1 index same oR
+        // elemet is in odd index and +1 is same  then distortion
+        // element is in left side
+        high = mid - 1;
+      }
+    }
+
+    return -1;
+  }
 }
