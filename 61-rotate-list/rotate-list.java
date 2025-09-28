a90539fee65cbd685fd5bc52e86ef06b45b469fb
Dipankar Dutta <deepankar.dutta1@gmail.com>
2025-09-28 11:16:50 +0530

    Time: 0 ms (100.00%) | Memory: 42.7 MB (50.05%) - LeetSync

--- 61-rotate-list/rotate-list.java
+++ 61-rotate-list/rotate-list.java
@@ -8,48 +8,40 @@
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
+
 class Solution {
-    public ListNode rotateRight(ListNode head, int k) {
 
-        int length=0;
-        ListNode temp=head;
-        ListNode tail=null;
-        if(head==null||head.next==null){
-            return head;
-        }
-        while(temp!=null){
-            if(temp.next==null){
-                tail=temp;
-            }
-            temp=temp.next;
-            length++;
-        }
-         k=k%length;
-        if(k==length||k==0){
-            return head;
-        }
-       
-        int limit=length-k;
-        int i=1;
-        temp=head;
-        while(i<limit){
-            temp=temp.next;
-            i++;
-        }
+  public ListNode rotateRight(ListNode head, int k) {
 
-        ListNode next=temp.next;
-
-        temp.next=null;
-        tail.next=head;
-        head=next;
-
-
-        return head;
-
-
-
-
-
-        
+    if (head == null) {
+      return null;
     }
+    ListNode curNode = head;
+    ListNode prev = null; // to help getting the lastNode will
+    int size = 0;
+    while (curNode != null) {
+      prev = curNode;
+      curNode = curNode.next;
+      size++;
+    }
+    k = k % size;
+    if (k == size || k == 0) return head;
+    curNode = head;
+
+    // will reach  node befor k
+    int i = 1;
+    while (curNode != null && i < size-k) {
+      curNode = curNode.next;
+      i++;
+    }
+    if (curNode == null) return head;
+    ListNode newHead = curNode.next; // get the newhead;
+    curNode.next = null; // to break the chain between k-1 and k
+    // simply point the last node to current head
+
+    prev.next = head;
+    head = newHead;
+
+    return head;
+  }
 }
