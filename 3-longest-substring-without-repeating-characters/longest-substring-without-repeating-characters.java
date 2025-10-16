8213fae390b461af240a65be27ff574d4539794a
Dipankar Dutta <deepankar.dutta1@gmail.com>
2025-10-16 12:54:08 +0530

    Time: 2 ms (98.75%) | Memory: 43.7 MB (90.84%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.java
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.java
@@ -1,58 +1,37 @@
 class Solution {
-    public int getMax(int max1,int max2){
-        if(max1>max2){
-            return max1;
-        }
-
-        return max2;
-    }
     public int lengthOfLongestSubstring(String s) {
-        if(s.length()==0 || s.length()==1){
-            return s.length();
-        }
-        int maxLength=Integer.MIN_VALUE;
-        HashMap<Character,Integer> preMap=new HashMap<>();
-        int low=0; // to keep track of sliding window left
-        int right=0; // to keep track of sliding window right  
-        // int si=0; // string index
-        while(low<=right && right<s.length() ){
-            if(!preMap.containsKey(s.charAt(right))){
-                // map doesn't contain add to map
-                preMap.put(s.charAt(right),right);
-                right++;
-                
-            }else{
-                // means it contains 
-                // get the maxLength 
-                maxLength=getMax(maxLength,(right-low));
-                System.out.println(maxLength);
-                // then find the index 
-                int getIndex=preMap.get(s.charAt(right));
-                // now check suppose low is already bigger than existing then ignore
-                if(getIndex>=low){
-                low=getIndex+1;
 
-                }
-                // next low must be next to getIndex
-                preMap.put(s.charAt(right),right);
-
-                if(right<low){
-                    right=low;
-                }else{
-                    right++;
-                }
-                // update the current in preMap with new duplicate index so that prev one get over written
+        if (s.length() == 0)
+            return 0;
+        if (s.length() == 1)
+            return 1;
+        int hash = 256;
+        int[] preMap = new int[hash];
+        Arrays.fill(preMap, -1);
+        int l = 0;
+        int r = 0;
+        int n = s.length();
+        int max_len = 0;
+        while (r < n) {
+            // if value already in the window bigger than l means in window 
+            if (preMap[s.charAt(r)] >= l) {
+                // already exist in window 
+                l++;
+                // keep on  moving to right to remove the exisiting character from window
+                // directly cant' move to l=r as dvdf r in d exist then i = r then v remvoed from max len then expected max len is 3 but output will be 2
+                continue;
             }
-            // right++;
+
+            // add in the ASCII value of the character : index   
+            // and will act as update if already exist
+            preMap[s.charAt(r)] = r;
+            // check each iteration for max_len
+            max_len = Math.max(max_len, r - l + 1);
+            r++;
+
         }
 
-        // one use case where there is no repeating character at all low at 0 right at last index + 1 
-        if(low==0 && right==s.length()){
-            return s.length();
-        }
-        if(maxLength<right-low){
-            return right-low;
-        }
-        return maxLength;
+        return max_len;
+
     }
 }
