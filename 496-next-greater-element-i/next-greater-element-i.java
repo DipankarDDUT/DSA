68dc1c7f11de3958e158501e4073a7b1951d8895
Dipankar Dutta <deepankar.dutta1@gmail.com>
2025-10-25 01:21:32 +0530

    Time: 2 ms (99.37%) | Memory: 43.9 MB (57.33%) - LeetSync

--- 496-next-greater-element-i/next-greater-element-i.java
+++ 496-next-greater-element-i/next-greater-element-i.java
@@ -1,31 +1,37 @@
 class Solution {
     public int[] nextGreaterElement(int[] nums1, int[] nums2) {
-        
+        // nums1 subset of nums2
 
-        // brute force O(num1 length * num2 lenght)  O(n2)
-        // for every num1 loop through nums2
-        // keep a flag when to look for comparison
-        // whenever num1 each found in num2 then toggle the flag to true then after wards look for comparison , after 
-        // end of iteration irrespective of toggling always toggel of the flag to false
-        int[] result=new int[nums1.length];
-        for(int i=0;i<nums1.length;i++){
-            int num=nums1[i];
-            boolean flag=false;
-            int greater=-1;
-            for(int j=0;j<nums2.length;j++){
-                if(nums2[j]==num){
-                    flag=true;
-                    continue;
-                }
-                if(flag){
-                    if(nums2[j]>num){
-                        greater=nums2[j];
-                        break;
-                    }
-                }
-            }
-            result[i]=greater;
+        int[] result = new int[nums1.length];
+        if (nums1.length > nums2.length)
+            return result;
+        // nums2 put in hashmap to get he index
+        // number index
+        HashMap<Integer, Integer> preMap = new HashMap<>();
+        for (int i = 0; i < nums2.length; i++) {
+            preMap.put(nums2[i], i);
         }
+
+        // Brute
+        for (int i = 0; i < nums1.length; i++) {
+            // for each element in nums1 check in larger set 
+            int max = -1;
+            // get the index in nums2 then start from next item
+            int index = preMap.get(nums1[i]);
+            int num = nums1[i];
+
+            for (int j = index + 1; j < nums2.length; j++) {
+                if (num < nums2[j]) {
+                    max = nums2[j]; // got the next max
+                    break;
+                }
+
+            }
+            result[i] = max;
+
+        }
+
         return result;
+
     }
 }
