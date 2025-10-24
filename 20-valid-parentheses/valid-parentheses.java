8c7fcd684c311dface7570254d3b18c532b0c9e9
Dipankar Dutta <deepankar.dutta1@gmail.com>
2025-10-24 16:17:49 +0530

    Time: 3 ms (59.19%) | Memory: 42.1 MB (25.27%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.java
+++ 20-valid-parentheses/valid-parentheses.java
@@ -5,13 +5,9 @@ class Solution {
 
         for (int i = 0; i < s.length(); i++) {
             char c = s.charAt(i);
-            if (c == '(') {
+            if (c == '('||c == '{'||c=='[') {
                 st.push(c);
-            } else if (c == '{') {
-              st.push(c);
-            } else if (c == '[') {
-              st.push(c);
-            } else if (c == ']') {
+            } else if (c==']') {
                 // suppose string start from closing bracket 
                 if (!st.isEmpty() && st.peek()=='[') {
                     st.pop();
