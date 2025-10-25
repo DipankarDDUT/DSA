93a3bbe3c2f411298f82f48f593f3d1b67ecb53d
Dipankar Dutta <deepankar.dutta1@gmail.com>
2025-10-25 20:56:31 +0530

    Time: 16 ms (59.90%) | Memory: 46.7 MB (16.67%) - LeetSync

--- 503-next-greater-element-ii/next-greater-element-ii.java
+++ 503-next-greater-element-ii/next-greater-element-ii.java
@@ -1,28 +1,25 @@
 class Solution {
-    public int[] nextGreaterElements(int[] nums) {
-        
-        Stack<Integer> st=new Stack<>();
-        int result[]=new int[nums.length];
-        for(int i=(2 * nums.length)-1;i>=0;i--){
-            // as usual start from last  1 2 1 1 2 1 last 3 are imaginary
-            int index=0;
-            if(i>=nums.length){
-                index=i-nums.length;
-            }else{
-                index=i;
-            }
-            while(!st.isEmpty() && st.peek()<=nums[index]){
-                st.pop();
-            }
-            // add to result only if nums.length
-            if(!st.isEmpty() && i<nums.length ){
-                    result[index]=st.peek();
-            }else if (st.isEmpty() && i<nums.length ){
-                result[index]=-1;
-            }
-            st.push(nums[index]);
-        }
+  public int[] nextGreaterElements(int[] arr) {
 
-        return result;
+    int[] result = new int[arr.length];
+    // number descending then all be -1 after this loop
+    Stack<Integer> stack = new Stack<>();
+    // hypothethical same array added as tail
+    for (int i = (2 * arr.length) - 1; i >= 0; i--) {
+      while (!stack.isEmpty() && stack.peek() <= arr[i%arr.length]) {
+        stack.pop();
+      }
+      if (i < arr.length) {
+        if (stack.isEmpty()) {
+          result[i] = -1;
+        } else {
+          result[i] = stack.peek();
+        }
+      }
+
+      stack.push(arr[i%arr.length]);
     }
+
+    return result;
+  }
 }
