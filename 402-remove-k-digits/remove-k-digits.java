4a402e61b5f231ca946151b5125bc0544ad423e4
Dipankar Dutta <deepankar.dutta1@gmail.com>
2025-10-29 09:35:18 +0530

    Time: 21 ms (53.88%) | Memory: 44.6 MB (92.26%) - LeetSync

--- 402-remove-k-digits/remove-k-digits.java
+++ 402-remove-k-digits/remove-k-digits.java
@@ -1,68 +1,53 @@
 class Solution {
-    public String removeKdigits(String num, int k) {
-        
+    public String removeKdigits(String nums, int k) {
+       
+        Stack<Character> stack=new Stack<>();
+        StringBuilder sb=new StringBuilder("");
 
-        // // lets assume its continuous  solution will work only 
-        // for continuos removal of number
-        // // we will take group of k starting from index 0 to find the largest and remove it 
-        // int max=Integer.MIN_VALUE;
-        // int starting=0;
-        // int ending=0;
-        // for(int i=0;i<num.length()-k+1;i++){
-        //     int tempNum=Integer.parseInt(num.substring(i,i+k));
-        //     if(tempNum>max){
-        //         max=tempNum;
-        //         starting=i;
-        //         ending=i+k;
-        //     }
-        //     if(i<num.length()-k && num.charAt(k)=='0' && i==0){
-        //         max=tempNum*10;
-        //     }
-        // }
-        // // StringBuilder sb=new StringBuilder();
-        // String result=num.substring(0,starting)+num.substring(ending);
-        // System.out.println(result);
-
-        // // remove all leading 0s
-        // StringBuilder sb=new StringBuilder(result); 
-        // while(sb.length()>0 && sb.charAt(0)=='0'){
-        //     sb.deleteCharAt(0);
-        // }
-        // result=sb.toString();
-        // return result.length()==0?"0":result;
-
-        // Algorithm : 
-        // empty stack check if at index character 0 skip else push
-        // if curr element is smaller than peek then keep on removing elemenet
-        // from stack until k>0 && current element < peek()
-        if(num.length()==1 && k==1){
-            return "0";
-        }
-            Stack<Character> st=new Stack<>();
-            int tempK=k;
-            for(int i=0;i<num.length();i++){
-
-                  while(!st.isEmpty() && num.charAt(i)<st.peek() && tempK>0){
-                            st.pop();
-                            tempK--;
-                        }
-                if(!st.isEmpty() || num.charAt(i)!='0'){
-                    // if not empty simply push the current after chcekign and removing 
-                    // or curr is not 0
-                        st.push(num.charAt(i));
-                }
+        if(k==nums.length()||nums.length()==0) return "0";
+       for(int i=0;i<nums.length();i++){
+            char c=nums.charAt(i);
+            // pop only when k>0 or else simply push 
+            while(!stack.isEmpty() && stack.peek()>c && k>0){
+                // all elements in stack larger or equal to nums[i]
+                // will be removed suppose stack has 4 5 1 now we got 2 
+                // 4 and 5 will be removed then we will get smaller total number 
+                stack.pop();
+                k--;
             }
-
-            while(tempK>0 && !st.isEmpty()){
-                st.pop();
-                            tempK--;  
+            // not to push the leading 0s as leading is non zero 
+            if(!(c=='0' && stack.isEmpty())){
+              stack.push(c);
             }
+       }
 
-             StringBuilder sb=new StringBuilder();
-             while(!st.isEmpty()){
-                sb.append(st.pop());
-             }
+       // k is still > 0 
+       // remove the last k element that is top of stack 
+       while(k>0 && !stack.isEmpty()
+       ){
+        char popValue=stack.pop();
+        k--;
+       }
 
-             return sb.length()==0?"0":sb.reverse().toString();
+       while(!stack.isEmpty()){
+         char popValue=stack.pop();
+           sb.append(popValue);
+       }
+
+       sb=sb.reverse();
+       // remove all leading zeroes
+       for(int i=0;i<sb.length();i++){
+            char c=sb.charAt(i);
+            if(c!='0'){
+                // means first non zero found break;
+                break;
+            }
+            // remve leading zeroes
+            sb.deleteCharAt(i);
+       }
+
+        // if after k remvoal nothing remains 
+       return sb.length()==0?"0":sb.toString();
+
     }
 }
