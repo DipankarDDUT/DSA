ac7a933da4779a8931efa33df1d27602ebc362b3
Dipankar Dutta <deepankar.dutta1@gmail.com>
2026-05-04 08:55:35 +0530

    Time: 2 ms (32.54%) | Memory: 47.4 MB (20.45%) - LeetSync

--- 45-jump-game-ii/jump-game-ii.java
+++ 45-jump-game-ii/jump-game-ii.java
@@ -1,21 +1,22 @@
 class Solution {
     public int jump(int[] nums) {
         
-        int furthest=0;
-        int currentEnd=0;
-        // whenever we can reach furthest we update jump
-        int jump=0;
+        int furthest = 0;
+        int currentEnd = 0;
+        int jump = 0;
 
-        for(int i=0;i<nums.length-1 && currentEnd<nums.length-1;i++){
-            furthest=Math.max(nums[i]+i,furthest);
-            if(i==currentEnd){
-                // for 1st index alwasy jump and currentEnd will get the furthest 
-                // whenever i reach to currentEnd means now we will make the jump
+        // traverse till second last index
+        for (int i = 0; i < nums.length - 1 && currentEnd < nums.length - 1; i++) {
+            
+            // update the furthest index reachable within current jump range
+            furthest = Math.max(nums[i] + i, furthest);
+
+            // if we reach the end of current jump range, we must jump
+            if (i == currentEnd) {
                 jump++;
-                currentEnd=furthest;
+                currentEnd = furthest; // move to next range
             }
         }
-
 
         return jump;
     }
